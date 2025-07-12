e36e9603766feda4ab50c66c5b11f191564563da
chenhaos112312 <158687105+chenhaos112312@users.noreply.github.com>
2025-07-12 11:02:03 +0800

    Add files via upload

--- src/components/widget/ProfileWidget.tsx
+++ src/components/widget/ProfileWidget.tsx
@@ -59,7 +59,8 @@ export const ProfileWidget = ({ data }: { data: ProfileWidgetType }) => {
         <div className="flex h-[72%] w-full flex-col items-start overflow-hidden px-3.5 pt-3.5 md:h-3/5 md:flex-row md:items-center md:justify-start md:space-x-3 md:px-3 md:py-2.5 lg:space-x-4 lg:px-5 lg:py-4">
           <div className="h-full overflow-hidden rotate-0 aspect-square rounded-2xl sm:mb-0 md:rounded-full">
             <ImageWithPlaceholder
-              src={data.logo.src}
+              // src={data.logo.src}
+              src={'https://notion-emojis.s3-us-west-2.amazonaws.com/prod/svg-twitter/1f9a7.svg'}
               width={data.logo.info.width}
               height={data.logo.info.height}
               blurDataURL={data.logo.info.placeholder}
@@ -69,10 +70,12 @@ export const ProfileWidget = ({ data }: { data: ProfileWidgetType }) => {
           </div>
           <div className="mt-2 flex flex-col justify-between gap-0.5 text-black dark:text-white md:mt-0">
             <h1 className="mb-2 text-base font-semibold tracking-tighter line-clamp-1 sm:mb-3 sm:text-base sm:tracking-normal md:mb-0 md:text-xl md:font-medium lg:text-2xl">
-              {data.name}
+              {/* {data.name} */}
+              {'FCSSR.全网档案管'}
             </h1>
-            <h2 className="hidden text-lg md:line-clamp-1 lg:text-xl">
-              {data.description}
+            <h2 className="hidden text-sm md:line-clamp-1 lg:text-sm">
+              {/* {data.description} */}
+               {'持续更新中！'}
             </h2>
           </div>
         </div>
@@ -83,32 +86,84 @@ export const ProfileWidget = ({ data }: { data: ProfileWidgetType }) => {
           )}
         >
           <div className="scrollbar-hide flex h-full w-full flex-row items-center justify-between gap-x-1.5 overflow-scroll px-3.5 pt-1 pb-3 font-medium md:justify-start md:gap-x-1 md:px-3 md:py-2 lg:gap-x-3 lg:px-5 lg:py-3 lg:text-sm">
-            {data.links.map((link, index) => (
-              <Link
-                key={link.link}
-                href={link.link}
+            {/* {data.links.map((link, index) => (
+              console.log(link,"-------------------")
+             
+            ))} */}
+             <Link
+                key={'aaa'}
+                href={'/about'}
                 target="_blank"
                 rel="noopener noreferrer"
                 className={classNames(
                   'leading-0 w-full transform cursor-pointer items-center justify-center rounded-lg text-white transition duration-300 ease-in-out hover:scale-95 md:h-full md:w-auto md:rounded-xl lg:rounded-2xl',
-                  link.id
+                  true
                     ? 'aspect-square md:aspect-auto md:gap-x-0.5 md:px-1.5 lg:gap-x-1.5 lg:px-3'
                     : 'aspect-square',
                   'bg-gradient-to-tr',
                   'from-neutral-300 dark:from-neutral-700',
                   'to-neutral-200 dark:to-neutral-600',
-                  index > 3 ? 'hidden md:flex' : 'flex'
+                   'flex'
                 )}
                 style={{
-                  backgroundImage: `linear-gradient(to top right, ${link.color_1}, ${link.color_2}) !important`,
+                  backgroundImage: `linear-gradient(to top right, '#0d1117', '#0d2235'}) !important`,
                 }}
               >
-                <LinkIcon icon={link.icon} hasId={!!link.id} />
+                <LinkIcon icon={'IoLogoGithub'} hasId={!!true} />
                 <p className="hidden md:block md:text-xs lg:text-base">
-                  {link.id}
+                  {'上车说明'}
                 </p>
               </Link>
-            ))}
+
+              <Link
+                key={'bbb'}
+                href={'/shouhou'}
+                target="_blank"
+                rel="noopener noreferrer"
+                className={classNames(
+                  'leading-0 w-full transform cursor-pointer items-center justify-center rounded-lg text-white transition duration-300 ease-in-out hover:scale-95 md:h-full md:w-auto md:rounded-xl lg:rounded-2xl',
+                  true
+                    ? 'aspect-square md:aspect-auto md:gap-x-0.5 md:px-1.5 lg:gap-x-1.5 lg:px-3'
+                    : 'aspect-square',
+                  'bg-gradient-to-tr',
+                  'from-neutral-300 dark:from-neutral-700',
+                  'to-neutral-200 dark:to-neutral-600',
+                   'flex'
+                )}
+                style={{
+                  backgroundImage: `linear-gradient(to top right, '#D7161C', '#FC281A'}) !important`,
+                }}
+              >
+                <LinkIcon icon={'/static/sspaiOutline.svg'} hasId={!!true} />
+                <p className="hidden md:block md:text-xs lg:text-base">
+                  {'推广赚钱'}
+                </p>
+              </Link>
+
+               <Link
+                key={'ccc'}
+                href={'/download'}
+                target="_blank"
+                rel="noopener noreferrer"
+                className={classNames(
+                  'leading-0 w-full transform cursor-pointer items-center justify-center rounded-lg text-white transition duration-300 ease-in-out hover:scale-95 md:h-full md:w-auto md:rounded-xl lg:rounded-2xl',
+                  true
+                    ? 'aspect-square md:aspect-auto md:gap-x-0.5 md:px-1.5 lg:gap-x-1.5 lg:px-3'
+                    : 'aspect-square',
+                  'bg-gradient-to-tr',
+                  'from-neutral-300 dark:from-neutral-700',
+                  'to-neutral-200 dark:to-neutral-600',
+                   'flex'
+                )}
+                style={{
+                  backgroundImage: `linear-gradient(to top right, '#0a69c6', '#0088fa'}) !important`,
+                }}
+              >
+                <LinkIcon icon={'IoLogoLinkedin'} hasId={!!true} />
+                <p className="hidden md:block md:text-xs lg:text-base">
+                  {'下载说明'}
+                </p>
+              </Link>
           </div>
         </div>
       </div>
