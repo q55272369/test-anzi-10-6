f2c674aaa800bb2dc8a25206f50c0cc718ef2e3b
q55272369 <q55272369@gmail.com>
2025-11-21 19:15:27 +0800

    Update StatsWidget.tsx

--- src/components/widget/StatsWidget.tsx
+++ src/components/widget/StatsWidget.tsx
@@ -19,7 +19,7 @@ export const StatsWidget = ({ data }: { data: BlogStats }) => {
         
            <div className='flex'>
            <span className="md:text-3xl md:font-medium lg:text-3xl mr-3 text-red-600">PRO+</span>
-           <span className="md:text-3xl md:font-medium  font-bold">商家计划已推出</span>
+           <span className="md:text-3xl md:font-medium  font-bold">联盟发展计划已推出</span>
            </div>
          
         </div>
@@ -31,7 +31,7 @@ export const StatsWidget = ({ data }: { data: BlogStats }) => {
         >
           <div className="scrollbar-hide flex h-full w-full flex-col items-center justify-between gap-x-1.5 overflow-scroll px-3.5 pt-1 pb-3 font-medium md:justify-start md:gap-x-1 md:px-3 md:py-2 lg:gap-x-3 lg:px-5 lg:py-3 lg:text-sm">
           <button onClick={()=>{window.open('https://pro-plus.top')}} type="submit" className="flex w-full justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-red-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-400" 
-              >立即加入</button>
+              >立即查看</button>
           </div>
         </div>
         </div>
